7cbe6aab70a5927373d8816fd2118757d8044e0a
Karl Winkler <karlw200@gmail.com>
2022-01-26 21:39:02 -0700

    updated to completed code

--- A1/proxy.c
+++ A1/proxy.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <string.h>
 #include <netdb.h>
+#include <sys/time.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "proxy_func.c"
@@ -16,12 +17,14 @@
 /* Main program for proxy */
 int main(int argc, char *argv[]){
     char message_in[MAX_MESSAGE_LENGTH], server_reply[MAX_MESSAGE_LENGTH];
-    char* proxy_response = REPLY;
+    // char* proxy_response = REPLY;
     struct sockaddr_in client;
     struct sockaddr_in proxy;
     struct sockaddr_in server;
     int accept_socket, client_socket, server_socket;
     socklen_t name_len;
+    int package_size = 0;
+    pid_t pid;
 
     // =====
     // proxy
@@ -52,7 +55,7 @@ int main(int argc, char *argv[]){
     
 
     // maintain the connection
-    for(;;){
+    while(1){
 
         name_len = sizeof(client);
         if((client_socket = accept(accept_socket, (struct sockaddr *)&client, &name_len)) < 0){
@@ -60,127 +63,146 @@ int main(int argc, char *argv[]){
             exit(1);
         }
 
-        // receive message from client
-        if(recv(client_socket, message_in, sizeof(message_in), 0) == -1){
-            puts("receive failed");
-            exit(1);
-        }
+        pid = fork();
 
-        // ======
-        // server
-        // ======
+        if(pid == 0){
+            close(accept_socket);
 
-        // get host from message
-        char temp[MAX_MESSAGE_LENGTH];
-        strcpy(temp, message_in);
-        char* host = get_header_contents(temp, "Host: ");
-        
-        // set up server settings to host
-
-        struct hostent *address;
-        address = gethostbyname(host);
-
-        //printf("%s", host);
-
-        bcopy((char *)address->h_addr, (char *)&server.sin_addr.s_addr, address->h_length);
-        // server.sin_addr.s_addr = inet_addr();
-        server.sin_family = AF_INET;
-        server.sin_port = htons(80);
-
-        //connect to socket
-        if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-            puts("failed to connect to accepting socket");
-            exit(1);
-        } 
-
-        // create connection to host
-        if(connect(server_socket, (struct sockaddr *)&server, sizeof(server)) < 0){
-            puts("connect to server error\n");
-            return 1;
-        }
-
-        printf("%s\n", message_in);
-
-        //send message to server
-        if(send(server_socket, message_in, sizeof(message_in), 0) < 0){
-            puts("message to server failed");
-            exit(1);
-        }
-
-
-        // receive message from server
-        if(recv(server_socket, server_reply, sizeof(server_reply), 0) == -1){
-            puts("receive from server failed");
-            exit(1);
-        }
-
-        // client
-        if(send(client_socket, server_reply, sizeof(server_reply), 0) < 0){
-                puts("return to client failed");
+            // receive message from client
+            if((package_size = recv(client_socket, message_in, sizeof(message_in), 0)) == -1){
+                puts("receive failed");
                 exit(1);
-        }
-
-        long int file_length = MAX_MESSAGE_LENGTH;
-        char* temp_header_content;
-
-        if((temp_header_content = get_header_contents(server_reply, "Content-Length: ")) != NULL){
-            int temp_file_length = atoi(temp_header_content);
-            if(temp_file_length > MAX_MESSAGE_LENGTH){
-                file_length = temp_file_length;
             }
-        }
 
-        long int bytes_sent = sizeof(server_reply);
+            // ======
+            // server
+            // ======
 
-        if(file_length > bytes_sent){
-            while(file_length > bytes_sent){
-                int package_size = 0;
-                
-                int current = 0;
-                // while(current < MAX_MESSAGE_LENGTH){
-                //     printf("%c", server_reply[current]);
-                //     current++;
-                // }
-                bzero(server_reply, sizeof(server_reply)); // clear string
-                if((package_size = recv(server_socket, server_reply, MAX_MESSAGE_LENGTH, 0)) == -1){
-                    puts("receive from server failed");
-                    exit(1);
+            // get host from message
+            char temp[MAX_MESSAGE_LENGTH];
+            strcpy(temp, message_in);
+            char* host = get_header_contents(temp, "Host: ");
+            
+            // set up server settings to host
+
+            struct hostent *address;
+            address = gethostbyname(host);
+
+            //printf("%s", host);
+
+            bcopy((char *)address->h_addr, (char *)&server.sin_addr.s_addr, address->h_length);
+            // server.sin_addr.s_addr = inet_addr();
+            server.sin_family = AF_INET;
+            server.sin_port = htons(80);
+
+            //connect to socket
+            if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+                puts("failed to connect to accepting socket");
+                exit(1);
+            } 
+
+            // create connection to host
+            if(connect(server_socket, (struct sockaddr *)&server, sizeof(server)) < 0){
+                puts("connect to server error\n");
+                return 1;
+            }
+
+            // printf("%s\n", message_in);
+
+            //send message to server
+            if(send(server_socket, message_in, MAX_MESSAGE_LENGTH, 0) < 0){
+                puts("message to server failed");
+                exit(1);
+            }
+
+            // while receiving message from server, send it on to the client
+            while((package_size = recv(server_socket, server_reply, sizeof(server_reply), 0)) > 0){
+
+                // gets the content type from the content type header
+                char temp[MAX_MESSAGE_LENGTH];
+                strcpy(temp, server_reply);
+                char* content_type = get_header_contents(temp, "Content-Type: ");
+                // puts(server_reply);
+                if(content_type != NULL){
+                    if(strcmp(content_type, "text/html; charset=UTF-8") == 0){
+                        
+                        do_the_funny(server_reply);
+                    }
+                    else if(strcmp(content_type, "image/jpeg") == 0){
+
+                        char response[100];
+                        char img_name[16];
+                        char img_size[16];
+                        int rand_val;
+                        struct timeval rand;
+
+                        gettimeofday(&rand, NULL);
+
+                        if((rand_val = rand.tv_usec % 3) == 0){
+                            strcpy(img_name, "clown.png");
+                            strcpy(img_size, "52930");
+                        }
+                        else if((rand_val = rand.tv_usec % 3) == 1){
+                            strcpy(img_name, "clown2.png");
+                            strcpy(img_size, "116686");
+                        }
+                        else if((rand_val = rand.tv_usec % 3) == 2){
+                            strcpy(img_name, "clown3.png");
+                            strcpy(img_size, "24761");
+                        }
+                        
+                        char * start = "HTTP/1.1 200 OK\r\nContent-Length: ";
+                        
+                        strcat(response, start);
+                        strcat(response, img_size);
+                        strcat(response, "\r\nConnection: close\r\nContent-Type: image/jpg\r\n\r\n");
+                        // HTTP/1.1 200 OK
+                        // Content-Length: 421
+                        // Connection: close
+                        // Content-Type: text/html; charset=UTF-8
+                        
+                        
+                        if(send(client_socket, response, strlen(response), 0) < 0){
+                            puts("message to server failed");
+                            exit(1);
+                        }
+
+                        //create buffer, FILE and byte variable
+                        FILE * fp;
+                        char buffer[MAX_LENGTH];
+                        int bytes = 0;
+                        
+                        //open file pointer
+                        fp = fopen(img_name, "rb");
+
+                        // read bits from image file and send them to the client
+                        while((bytes = fread(buffer, 1, MAX_LENGTH, fp)) > 0){
+                            //puts("buffer");
+                            if(send(client_socket, buffer, bytes, 0) < 0){
+                                puts("message to server failed");
+                                exit(1);
+                            }
+                        }
+                        break;
+                    }
                 }
 
-                // printf("\n========%ld=======\n========%ld/%ld=========", package_size, bytes_sent, file_length);
-                // ======
-                // client
-                // ======
                 if(send(client_socket, server_reply, package_size, 0) < 0){
                     puts("return to client failed");
                     exit(1);
                 }
-                bytes_sent += package_size; // increment bytes sent
+                bzero(server_reply, sizeof(server_reply)); // clear string
+                // sleep(0.01);
             }
+
+            close(client_socket);
+            exit(0);
+            
         }
-        else{
-            // ======
-            // client
-            // ======
-            // if(send(client_socket, server_reply, sizeof(server_reply), 0) < 0){
-            //     puts("return to client failed");
-            //     exit(1);
-            // }
-        }
-
-
-        // replace_words(message_in);
-        
-        printf("%s\n", server_reply);
-        // bzero(message_in, MAX_MESSAGE_LENGTH);
-
-        puts("end");
 
     }
 
-    close(accept_socket);
-    close(client_socket);
-    puts("conection ended");
+    puts("conection ended"); // shouldnt ever really reach here I think
     return 0;
     
 
--- A1/proxy_func.c
+++ A1/proxy_func.c
@@ -1,27 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // for handling requests so I dont have to dig through server connecting shit that looks the same as everything else
 // I want to be able to code it seperately, I can combine it later if I need to 
 // hopefuly this doesnt create more problems
 // :)
 
-#define STRING_WORDS 2000
-
-// char* replace_words(char string[STRING_WORDS]){
-//     for(int i = 0; i < STRING_WORDS; i++){
-//         if(strcmp(string[i], "\n\r\n\r") == 0){ // \n\r\n\r is EOF
-//             break; // exit loop
-//         }
-
-//         if(strcmp(string[i], "Happy") == 0){
-//             strcpy(string[i], "Silly");
-//         }
-//     }
-// }
+#define MAX_LENGTH 5068 // same as max message length
+#define CLOWN_FILE_LENGTH 52930
 
 char* get_header_contents(char* message, char* header){
     // message should be the HTTP message
     // header should be in the form "Header: "
-
     char* res; // gets the header 
+
 
     if((res = strstr(message, header)) == NULL){
         return NULL;
@@ -32,4 +25,60 @@ char* get_header_contents(char* message, char* header){
     res = res+strlen(header);  // moves pointer to the start of contents    
 
     return res;
+}
+
+char* do_the_funny(char* message){
+    
+    char *res = (strstr(message, "\r\n\r\n")); // move to contents
+    while((res = strstr(res, "appy")) != NULL){ //change each char
+        if(res[-1] == 'h'){
+            res[-1] = 's';
+        }
+        else if(res[-1] == 'H'){
+            res[-1] = 'S';
+        }
+        else{
+            break;
+        }
+        res[0] = 'i';
+        res[1] = 'l';
+        res[2] = 'l';
+        res[3] = 'y';
+        res += 5;
+    }
+    return message;
+}
+
+void send_clown(int socket){
+
+    FILE * fp;
+    char buffer[MAX_LENGTH];
+    int bytes = 0;
+
+    fp = fopen("clown.png", "rb");
+
+    char * response = "HTTP/1.1 200 OK\r\nContent-Length: 52930\r\nConnection: close\r\nContent-Type: image/jpg\r\n\r\n";
+    // HTTP/1.1 200 OK
+    // Content-Length: 421
+    // Connection: close
+    // Content-Type: text/html; charset=UTF-8
+
+    bytes = fread(buffer, 1, MAX_LENGTH - strlen(response), fp);
+
+    strcat(response, buffer);
+
+    printf("%s", response);
+
+    if(send(socket, response, MAX_LENGTH, 0) < 0){
+        puts("message to server failed");
+        exit(1);
+    }
+
+    while((bytes = fread(buffer, 1, MAX_LENGTH, fp)) > 0){
+        puts("read");
+        if(send(socket, buffer, bytes, 0) < 0){
+            puts("message to server failed");
+            exit(1);
+        }
+    }
 }
